12ecee6a815dfa9a4280565c70007d76428f9181
Nayun Kim <ny.kim@tum.de>
2023-10-30 22:26:54 +0100

    CSS in Javascript

--- practiceWeb/app.js
+++ practiceWeb/app.js
@@ -1,26 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    h1.style.color ="blue";
-}
-function handleMouseEnter(){
-   h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave(){
-   h1.innerText = "Mouse is gone!";
-}
+   if(h1.style.color === "blue") {
+      h1.style.color = "tomato";
+   } else {
+      h1.style.color = "blue";
+   }
+};
 
-function handleWindowResize(){
-   document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-   alert("no!");
-}
 h1.addEventListener("click", handleTitleClick);
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
